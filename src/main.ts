85164b9805d3c3967710fc784594ed6725c1dbd6
akeyblack <bladiclab16@gmail.com>
2022-08-15 18:21:43 +0300

    heroku commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const env = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(env.PORT ?? 3000);
 }
 bootstrap();
